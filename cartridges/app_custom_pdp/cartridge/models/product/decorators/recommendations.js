a64d1770bec9b0428462210717cb0a60c9be9bdd
AngelManchevC4 <angel.manchev@c4nexus.com>
2023-12-15 11:11:07 +0200

    Code, refactored, cleared

--- cartridges/app_custom_pdp/cartridge/models/product/decorators/recommendations.js
+++ cartridges/app_custom_pdp/cartridge/models/product/decorators/recommendations.js
@@ -1,19 +1,6 @@
 'use strict'
 
 module.exports = function (object, product) {
-
-    // var ProductFactory = require('*/cartridge/scripts/factories/product');
-    // var productRecommendations = product.recommendations;
-    // var recommendations = [];
-
-    // for (let i = 0; i < productRecommendations.length; i++) {
-    //     recommendations.push(ProductFactory.get({ pid: productRecommendations[i].recommendedItemID, pview: 'tile' }))
-    // }
-
-    // productRecommendations.map(product => {
-    //     recommendations.push(ProductFactory.get({ pid: product.recommendedItemID, pview: 'tile' }))
-    // })
-
     Object.defineProperties(object, {
         recommendations: {
             enumerable: true,
